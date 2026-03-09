61b8b01751608e432f0d8fdc0f62a7b69f2a1ebd
Himanshu Thakur <himanshu97687@gmail.com>
2026-03-09 14:42:34 +0530

    connect app to first

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template('first.html')
+
+@app.route("/home")
+def home():
     return render_template('index.html')
 
 @app.route("/analyzer")
